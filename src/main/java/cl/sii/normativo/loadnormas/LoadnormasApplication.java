1efbe1a4735db5c4956f302b48f746faea1ae6fb
Alberto Carlos Michell San Martin Concha <alberto.sanmartin@sii.cl>
2025-04-24 16:42:15 -0400

    primer commit

---
+++ src/main/java/cl/sii/normativo/loadnormas/LoadnormasApplication.java
@@ -0,0 +1,13 @@
+package cl.sii.normativo.loadnormas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadnormasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadnormasApplication.class, args);
+	}
+
+}
