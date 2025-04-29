5b8da7fc77a1b139a22344ea56ebf2992589b035
Altobert <alberto.sanmartin@usach.cl>
2025-04-28 22:55:04 -0400

    se agregan archivos a indice

--- src/main/java/cl/sii/normativo/loadnormas/LoadnormasApplication.java
+++ src/main/java/cl/sii/normativo/loadnormas/LoadnormasApplication.java
@@ -2,8 +2,10 @@ package cl.sii.normativo.loadnormas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class LoadnormasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/cl/sii/normativo/loadnormas/controller/DocumentController.java
+++ src/main/java/cl/sii/normativo/loadnormas/controller/DocumentController.java
@@ -1,11 +1,7 @@
 package cl.sii.normativo.loadnormas.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,7 +26,8 @@ public class DocumentController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadPDF(@RequestParam("file") MultipartFile file) {
+    //@RequestParam("file")
+    public ResponseEntity<String> uploadPDF(@RequestBody MultipartFile file) {
         try {
             // Extraer texto del PDF
             String content = PDFTextExtractor.extractText(file.getInputStream());
