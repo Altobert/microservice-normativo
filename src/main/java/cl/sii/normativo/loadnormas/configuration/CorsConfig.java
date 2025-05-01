1f72087d9bd015da97950e42c2815832a53aae78
Altobert <alberto.sanmartin@usach.cl>
2025-05-01 15:18:51 -0400

    se agregan documentos

--- src/main/java/cl/sii/normativo/loadnormas/configuration/CorsConfig.java
+++ src/main/java/cl/sii/normativo/loadnormas/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001") // Next.js default port
+                        .allowedOrigins("http://localhost:3000") // Next.js default port
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- src/main/java/cl/sii/normativo/loadnormas/controller/DocumentController.java
+++ src/main/java/cl/sii/normativo/loadnormas/controller/DocumentController.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import cl.sii.normativo.loadnormas.services.LuceneIndexer;
 import cl.sii.normativo.loadnormas.services.PDFTextExtractor;
 
-
 @RestController
 @RequestMapping("/api/documents")
 public class DocumentController {
